a9badd7f38f5e78c723e85d70aa90af35f96f700
Rupert Bedford <rupert@rupertb.com>
2018-06-16 19:20:56 +0100

    Add toml

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Rupert Bedford",
     author_email="rupert@rupertb.com",
     packages=find_packages(exclude=["tests"]),
-    install_requires=["python-language-server", "black"],
+    install_requires=["python-language-server", "black", "toml"],
     extras_require={"dev": ["isort", "flake8", "pytest"]},
     entry_points={"pyls": ["pyls_black = pyls_black.plugin"]},
     classifiers=(
