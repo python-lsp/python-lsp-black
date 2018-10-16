01f16eff9d7c580ce7fe17a17b9fe22c955f8313
Rupert Bedford <rupert@rupertb.com>
2018-10-17 00:08:58 +0100

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pyls-black",
-    version="0.2.1",
+    version="0.3.0",
     description="Black plugin for the Python Language Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
