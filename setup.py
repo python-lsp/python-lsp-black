55a8e0351f69104a9e4463697a9803d9aab6e974
Rupert Bedford <rupert@rupertb.com>
2020-07-03 18:36:25 +0100

    Add setup.cfg (#26)
    * Moved the metadata into setup.cfg
    
    * Re-add tool config
    
    * Add min black version
    
    Co-authored-by: KOLANICH <kolan_n@mail.ru>

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(
-    name="pyls-black",
-    version="0.4.4",
-    description="Black plugin for the Python Language Server",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rupert/pyls-black",
-    author="Rupert Bedford",
-    author_email="rupert@rupertb.com",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=["python-language-server", "black>=19.3b0", "toml"],
-    extras_require={"dev": ["isort", "flake8", "pytest", "mypy", "pytest"]},
-    entry_points={"pyls": ["pyls_black = pyls_black.plugin"]},
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
-)
+if __name__ == "__main__":
+    setup()
